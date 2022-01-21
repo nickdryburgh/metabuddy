d08c13a44b1f573807b8c1442fcaa4d47fb3cb0e
nickdryburgh <nickdryburgh@gmail.com>
2022-01-21 10:55:07 +0000

    Further test
    Added stubbed API query
    Attempt to edit page icon
    Page title update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // document.write("MetaBuddy processing...");
+
+  // console.log('Before');
+
+  // const request = new XMLHttpRequest()
+  // request.open('GET', 'https://api.decentraland.org/v2/tiles');
+  // //request.responseType = 'blob'
+  // request.addEventListener('load', () => {
+
+  //   if (request.status == 200) { 
+  //     //var responseString = request.response.JSON;
+  //     var dataBlob = JSON.parse(request.response);
+  //     console.log('Success');
+  //     const dataArray = JSON.parse(request.response);
+      
+  //     const tileBlob = dataBlob.data['30,100'];
+
+  //     console.log(tileBlob);
+
+  //     document.write(tileBlob['name']);
+  //   } else {
+  //     console.log(`${request.status}: ${request.statusText}`)
+  //   }
+  // })
+  // request.addEventListener('error', event => console.log('Network error'));
+  // request.send()
+
+  // console.log('After');
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +46,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React if you want
+          Learn React 
         </a>
       </header>
     </div>
---
+++ src/images.js
@@ -0,0 +1,188 @@
+// echo -n
+
+'use strict'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.write("MetaBuddy processing...");
+
+  console.log('Before');
+
+  const request = new XMLHttpRequest()
+  request.open('GET', 'https://api.decentraland.org/v2/tiles');
+  //request.responseType = 'blob'
+  request.addEventListener('load', () => {
+
+    if (request.status == 200) { 
+      //var responseString = request.response.JSON;
+      var dataBlob = JSON.parse(request.response);
+      console.log('Success');
+      const dataArray = JSON.parse(request.response);
+      
+      const tileBlob = dataBlob.data['30,100'];
+
+      console.log(tileBlob);
+
+      document.write(tileBlob['name']);
+    } else {
+      console.log(`${request.status}: ${request.statusText}`)
+    }
+  })
+  request.addEventListener('error', event => console.log('Network error'));
+  request.send()
+
+  console.log('After');
+
+})
+
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}
+      />
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
+    const current = history[history.length-1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+        squareIndex: i
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0,
+    })
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const col = history[move].squareIndex % 3;
+      const row = 2 - (history[move].squareIndex - col) / 3;
+      const desc = move ?
+        'Go to move #' + move + ' ('+col+', '+row+ ') ' :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    })
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
