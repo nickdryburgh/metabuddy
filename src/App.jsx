094bfade55b8c679e2649c9a44641f23018d3814
nickdryburgh <nickdryburgh@gmail.com>
2022-01-21 18:25:27 +0000

    output printed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import './App.css';
 import Form from './Form';
 import PropTypes from 'prop-types';
 
-const App = () => {
-  let output = "empty";
 
-  const handleChange = val => {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      output: "nothing"
+    }
+  }
+
+  handleChange = val => {
     console.log("new data x:"+ val.xcoord + " y:" + val.ycoord);
-
+ 
     const request = new XMLHttpRequest()
     request.open('GET', 'https://api.decentraland.org/v2/tiles');
 
@@ -19,7 +25,7 @@ const App = () => {
         var dataBlob = JSON.parse(request.response);
         const key = val.xcoord + "," + val.ycoord;
         const tileBlob = dataBlob.data[key];
-        //this.output = tileBlob['name'];
+        this.setState({output: tileBlob['name']});
         console.log(tileBlob['name']);
       } else {
         console.log(`${request.status}: ${request.statusText}`)
@@ -30,21 +36,23 @@ const App = () => {
     request.send()
   };
 
-  return (
-    <div className='container'>
-      <div className='row center'>
-        <h1 className='white-text'> Metabuddy </h1>
-      </div>
-      <div className='row'>
-        <div className='col m12 s12'>
-          <Form change={handleChange} />
+  render() {
+    return  (
+      <div className='container'>
+        <div className='row center'>
+          <h1 className='white-text'> Metabuddy </h1>
+        </div>
+        <div className='row'>
+          <div className='col m12 s12'>
+            <Form change={this.handleChange} />
+          </div>
+        </div>
+        <div className='row center'>
+          <h4 className='white-text'>{this.state.output}</h4>
         </div>
       </div>
-      <div className='row center'>
-        <h4 className='white-text'>{output}</h4>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 // App.propTypes = {
