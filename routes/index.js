26871efc4716e7103c09fe62d436e1e6837b803b
nickdryburgh <nickdryburgh@gmail.com>
2022-02-01 14:36:49 +0000

    pick random parcel

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,18 @@ router.get('/', function(req, res, next) {
 
 async function dclFetch(res) {
   console.log("Fetch Tiles...")
+  const xCoord = -150 + Math.round(Math.random() * 300);
+  const yCoord = -150 + Math.round(Math.random() * 300);
   const options = {method: 'GET', headers: {Accept: 'application/json'}};
   await fetch('https://api.decentraland.org/v2/tiles', options)
   .then(response => response.json())
   .then(jsondata => {
     console.log("------------------------------------------------------------------");
     console.log("data...")
-    const key = "9,11";
+    const key = xCoord + "," + yCoord;
     const tileBlob = jsondata.data[key];
     console.log(tileBlob['name']);
-    res.send("parcal name is: "+tileBlob['name']);
+    res.send("Parcel name at "+xCoord+","+yCoord+" is: "+tileBlob['name']);
   })
   .catch(err => console.error(err));
 }
