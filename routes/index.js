cc46ec3dc99385f046405cf9243a9b23dc740e98
nickdryburgh <nickdryburgh@gmail.com>
2022-02-01 12:46:17 +0000

    switch to express app

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NExpress' });
+});
+
+async function dclFetch(res) {
+  console.log("Fetch Tiles...")
+  const options = {method: 'GET', headers: {Accept: 'application/json'}};
+  await fetch('https://api.decentraland.org/v2/tiles', options)
+  .then(response => response.json())
+  .then(jsondata => {
+    console.log("------------------------------------------------------------------");
+    console.log("data...")
+    const key = "9,11";
+    const tileBlob = jsondata.data[key];
+    console.log(tileBlob['name']);
+    res.send(tileBlob['name']);
+  })
+  .catch(err => console.error(err));
+}
+
+
+router.get('/t/', function(req, res, next) {
+
+  dclFetch(res);
+
+
+});
+
+module.exports = router;
